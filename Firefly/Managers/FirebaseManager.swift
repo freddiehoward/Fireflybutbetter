d18b47b54ee373147cc8f8f4a01ef1d063aa691e
freddie <howard.f@etoncollege.org.uk>
2024-06-14 11:20:58 +0100

    j copid the code but will undersyand it later

---
+++ Firefly/Managers/FirebaseManager.swift
@@ -0,0 +1,8 @@
+//
+//  FirebaseManager.swift
+//  Firefly
+//
+//  Created by Howard, Freddie (ABH) on 14/06/2024.
+//
+
+import Foundation
---
+++ Firefly/Models/Todo.swift
@@ -0,0 +1,8 @@
+//
+//  Todo.swift
+//  Firefly
+//
+//  Created by Howard, Freddie (ABH) on 14/06/2024.
+//
+
+import Foundation
