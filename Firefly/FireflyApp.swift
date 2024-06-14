95e54179e541bc42afe1a9e611dfe544bf2ced45
freddie <howard.f@etoncollege.org.uk>
2024-06-14 09:52:25 +0100

    Initial Commit

---
+++ Firefly/FireflyApp.swift
@@ -0,0 +1,17 @@
+//
+//  FireflyApp.swift
+//  Firefly
+//
+//  Created by Howard, Freddie (ABH) on 14/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct FireflyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
